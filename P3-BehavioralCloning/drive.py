88852841df2ade7c84f738b6bd0b41a0be0671a8
Vilas Chitrakaran <vilas.chitrakaran@dyson.com>
2017-02-13 13:19:58 +0000

    added cropping layer

--- P3-BehavioralCloning/drive.py
+++ P3-BehavioralCloning/drive.py
@@ -19,9 +19,6 @@ app = Flask(__name__)
 model = None
 prev_image_array = None
 
-def crop_image(image, rowmin, rowmax):
-    return image[rowmin:rowmax,:,:]
-
 @sio.on('telemetry')
 def telemetry(sid, data):
     if data:
@@ -36,7 +33,6 @@ def telemetry(sid, data):
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         #image = image.convert('YCbCr')
         image_array = np.asarray(image)
-        image_array = crop_image(image_array, 80, 160)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
         throttle = 0.2
         print(steering_angle, throttle)
