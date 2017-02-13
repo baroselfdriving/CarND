0774474e24c337f1d9f11ad919a7b6222b73001a
Vilas Chitrakaran <vilas.chitrakaran@dyson.com>
2017-02-13 13:57:28 +0000

    added rgb to yuv conversion

--- P3-BehavioralCloning/drive.py
+++ P3-BehavioralCloning/drive.py
@@ -31,7 +31,7 @@ def telemetry(sid, data):
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
-        #image = image.convert('YCbCr')
+        image = image.convert('YCbCr')
         image_array = np.asarray(image)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
         throttle = 0.2
