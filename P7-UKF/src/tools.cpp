357c1817446515a60fc2b1cd1c3d39acf082c17b
Vilas Chitrakaran <cvilas@gmail.com>
2017-05-25 08:30:57 +0100

    rmse done, project compiles

--- P7-UKF/src/tools.cpp
+++ P7-UKF/src/tools.cpp
@@ -10,9 +10,37 @@ Tools::Tools() {}
 Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+                              const vector<VectorXd> &ground_truth)
+{
+    VectorXd rmse = VectorXd::Zero( 4 );
+    const size_t estSz = estimations.size();
+    // check the validity of the following inputs:
+    //  * the estimation vector size should not be zero
+    //  * the estimation vector size should equal ground truth vector size
+    if(estSz != ground_truth.size() || estSz == 0)
+    {
+        std::cerr << "[CalculateRMSE] Invalid estimation or ground_truth data" << std::endl;
+        return rmse;
+    }
+
+
+    //accumulate squared residuals
+    for(unsigned int i=0; i < estSz; ++i)
+    {
+
+        VectorXd residual = estimations[i] - ground_truth[i];
+
+        //coefficient-wise multiplication
+        residual = residual.array()*residual.array();
+        rmse += residual;
+    }
+
+    //calculate the mean
+    rmse = rmse/estSz;
+
+    //calculate the squared root
+    rmse = rmse.array().sqrt();
+
+    //return the result
+    return rmse;
 }
