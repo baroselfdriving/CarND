8cff6d8e2f292bac55cf57e4ada41bfe76cb3e9d
vilas chitrakaran <cvilas@gmail.com>
2017-07-06 11:25:15 +0100

    pf done. passes tests

--- P8-PF/src/particle_filter.cpp
+++ P8-PF/src/particle_filter.cpp
@@ -27,21 +27,21 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 
     // create random distribution centered around initial position. Assume std[] has length = 3
-    default_random_engine rand_gen;
-    normal_distribution<double> distrib_x(x, std[0]);
-    normal_distribution<double> distrib_y(y, std[1]);
-    normal_distribution<double> distrib_theta(theta, std[2]);
+    default_random_engine randGen;
+    normal_distribution<double> distribX(x, std[0]);
+    normal_distribution<double> distribY(y, std[1]);
+    normal_distribution<double> distribTheta(theta, std[2]);
 
-    num_particles = 1000;
+    num_particles = 100;
     particles = std::vector<Particle>(num_particles);
     int id = 0;
     for(auto& particle : particles)
     {
         particle.id = id;
         id++;
-        particle.x = distrib_x(rand_gen);
-        particle.y = distrib_y(rand_gen);
-        particle.theta = distrib_theta(rand_gen);
+        particle.x = distribX(randGen);
+        particle.y = distribY(randGen);
+        particle.theta = distribTheta(randGen);
         particle.weight = 1;
     }
     is_initialized = true;
@@ -55,10 +55,10 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	//  http://www.cplusplus.com/reference/random/default_random_engine/
 
     // For motion noise, create random distribution centered around 0. Assume std_pos[] has length = 3
-    default_random_engine rand_gen;
-    normal_distribution<double> distrib_x(0, std_pos[0]);
-    normal_distribution<double> distrib_y(0, std_pos[1]);
-    normal_distribution<double> distrib_theta(0, std_pos[2]);
+    default_random_engine randGen;
+    normal_distribution<double> distribX(0, std_pos[0]);
+    normal_distribution<double> distribY(0, std_pos[1]);
+    normal_distribution<double> distribTheta(0, std_pos[2]);
 
     // propagate motion
     const double EEPS = 1e-6;
@@ -82,9 +82,9 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
         }
 
         // add motion noise
-        particle.x += distrib_x(rand_gen);
-        particle.y += distrib_y(rand_gen);
-        particle.theta += distrib_theta(rand_gen);
+        particle.x += distribX(randGen);
+        particle.y += distribY(randGen);
+        particle.theta += distribTheta(randGen);
     }
 
 }
@@ -127,7 +127,89 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   3.33
 	//   http://planning.cs.uiuc.edu/node99.html
 
+    // Steps:
+    // - find predicted observations within sensor range
+    // - associate those predictions with actual observations
+    // - calculate weight as product of pdf(z_k; z_k*, sigma) for all observations k
 
+    // assume measurement axes are uncorrelated, ie cov_xy = 0
+    // since particle weight calculations are based on range, use std_landmark[0] for range measurement uncertainty
+    // we dont use std_landmark[1], which is bearing measurement uncertainty
+    const double scaledVar = -0.5/(std_landmark[0] * std_landmark[0]);
+
+    for(auto& particle : particles)
+    {
+        //----------------------------
+        // predictions within sensor range
+        //
+        // Landmark in particle coordinates: T_pl = Rt * (T_l - T_p) = Rt * (T_plm)
+        //  where   T_l: landmark in map coordinates
+        //          T_p: particle in map coordinates
+        //          Rt: transpose of particle orientation in the map
+        //          T_plm: relative position of landmark to particle in map coordinates
+        //----------------------------
+
+        // elements of rotation matrix
+        const double cp = cos(particle.theta);
+        const double sp = sin(particle.theta);
+
+        std::vector<LandmarkObs> predicted; // list of predicted landmarks
+        for(auto& landmark : map_landmarks.landmark_list)
+        {
+            // landmark relative to particle in map coordinates
+            double T_plm_x = landmark.x_f - particle.x;
+            double T_plm_y = landmark.y_f - particle.y;
+
+            double distToLandmark = sqrt(T_plm_x*T_plm_x + T_plm_y*T_plm_y);
+            if(distToLandmark < sensor_range)
+            {
+                // transform relative landmark pose to particle coordinates
+                LandmarkObs prediction;
+                prediction.id = landmark.id_i;
+                prediction.x = cp * T_plm_x + sp * T_plm_y;
+                prediction.y = -sp * T_plm_x + cp * T_plm_y;
+                predicted.push_back(prediction);
+            }
+        } // predictions
+
+        //----------------------------
+        // associate observations with predictions
+        //----------------------------
+
+        // note: we know we only modify the landmark ID of observations. So it's ok to
+        // directly modify 'observations' vector. Work with a copy if observations are
+        // modified in any other way
+        dataAssociation(predicted, observations);
+
+        //----------------------------
+        // calculate weight of the particle
+        //
+        //  weight = PROD[ {1/sqrt(2 * pi * det(cov))} * exp(0.5 * d_i^T * cov^-1 * d_i) ]
+        //          where   PROD denotes cumulative product over all i
+        //                  d_i = relative pose of landmark = observation - prediction
+        //                  cov = measurement covariance
+        //                  det(cov) = determinant of measurement covariance
+        // we can safely ignore the first term, which is a system constant (does not change for every observation)
+        // as the scale will not effect particle resampling by weight. Therefore the weight is a product of
+        // exponentials alone.
+        //----------------------------
+        particle.weight = 1; //initialise
+        for(auto& landmarkSeen : observations)
+        {
+            // find the prediction for this observation
+            for(auto& landmarkPred : predicted)
+            {
+                if( landmarkPred.id == landmarkSeen.id )
+                {
+                    // update particle weight by accumulating observation weight
+                    const double dx = landmarkSeen.x - landmarkPred.x;
+                    const double dy = landmarkSeen.y - landmarkPred.y;
+                    const double d = scaledVar * (dx * dx + dy * dy);
+                    particle.weight *= exp(d);
+                }
+            }
+        } // weight update
+    } // for each particle
 }
 
 void ParticleFilter::resample() {
@@ -135,6 +217,25 @@ void ParticleFilter::resample() {
 	// NOTE: You may find std::discrete_distribution helpful here.
 	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
 
+    // create a vector of weights alone
+    std::vector<double> weights;
+    for(const auto& particle : particles)
+    {
+        weights.push_back(particle.weight);
+    }
+
+    // create a random number generator that chooses a particle in proportion to its weight
+    std::random_device randDev;
+    std::mt19937 randGen(randDev());
+    std::discrete_distribution<> distrib(weights.begin(), weights.end());
+
+    std::vector<Particle> selectedParticles;
+    for(int i = 0; i < particles.size(); ++i)
+    {
+        selectedParticles.push_back( particles[distrib(randGen)] );
+    }
+
+    particles = selectedParticles;
 }
 
 Particle ParticleFilter::SetAssociations(Particle particle, std::vector<int> associations, std::vector<double> sense_x, std::vector<double> sense_y)
