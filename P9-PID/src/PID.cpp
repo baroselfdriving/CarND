4161540b026bd04a465ffda6dd53688be3cff054
vilas chitrakaran <cvilas@gmail.com>
2017-07-06 16:32:38 +0100

    basic pid hand-tuned

--- P9-PID/src/PID.cpp
+++ P9-PID/src/PID.cpp
@@ -6,16 +6,28 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() : Kp(0), Ki(0), Kd(0){}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Kd = Kd;
+    this->Ki = Ki;
+
 }
 
 void PID::UpdateError(double cte) {
+    static double lastCte = cte;
+    static double sumCte = 0;
+    p_error = -Kp * cte;
+    i_error = -Ki * sumCte;
+    d_error = -Kd * (cte - lastCte);
+    lastCte = cte;
+    sumCte += cte;
 }
 
 double PID::TotalError() {
+    return p_error + i_error + d_error;
 }
 
