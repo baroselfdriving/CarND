78fd7688656ebf294c46770de8bdf80c9a5ee975
vilas chitrakaran <cvilas@gmail.com>
2017-07-15 19:57:14 +0100

    started report

--- P10-MPC/src/main.cpp
+++ P10-MPC/src/main.cpp
@@ -146,7 +146,7 @@ int main() {
           // Feel free to play around with this value but should be to drive
           // around the track with 100ms latency.
           //
-          //\todo NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE SUBMITTING.
+          ///\todo NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE SUBMITTING.
           this_thread::sleep_for(chrono::milliseconds(100));
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
